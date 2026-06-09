6befc740e2cf5ba46d4b73a3e2276e1fa73cb0ac
Saibalajiiii <131664467+Saibalajiiii@users.noreply.github.com>
2026-06-09 22:52:44 +0530

    Add files via upload

---
+++ rtl/ Verilog source files/tb.v
@@ -0,0 +1,36 @@
+
+`timescale 1ns/1ps
+`include "d_ff.v"
+`include "wtm16x16_complete.v"
+`include "filter_64.v"
+module tb;
+
+reg ce,rst,clk;
+reg [15:0]x;
+wire [31:0]y;
+
+filter_64 uut(ce,rst,x,clk,y);
+initial begin
+clk=0;
+ end
+always #5 clk=~clk;
+
+initial begin
+rst=1'b1;
+#50
+rst=0;
+x=16'b0;
+#650
+$display("x=%d ,y=%d",$signed(x),$signed(y));
+x=16'b01;
+#650
+$display("x=%d ,y=%d",$signed(x),$signed(y));
+$finish;
+end
+
+
+initial begin
+$fsdbDumpfile("test1.fsdb");
+$fsdbDumpvars(0,tb);
+end
+endmodule
